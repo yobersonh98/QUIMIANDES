1060f3c7f547f543e2b0a765a80d586671fc1447
gpaez <yuniorpaezpabon@gmail.com>
2025-02-17 23:59:32 -0500

    modulo de pedidos

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,9 +7,10 @@ import { ConfigModule } from '@nestjs/config';
 import { ClienteModule } from './cliente/cliente.module';
 import { CotizacionModule } from './cotizacion/cotizacion.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PedidoModule } from './pedido/pedido.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ConfigModule.forRoot(), ClienteModule, CotizacionModule, PrismaModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot(), ClienteModule, CotizacionModule, PrismaModule, PedidoModule],
   controllers: [AppController],
   exports: [PrismaModule],
   providers: [AppService],
